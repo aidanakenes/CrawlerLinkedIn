3ac770b671521a991647b34d851849b709fb6b33
Aidana Kenes <aidanakenesova30@gmail.com>
2020-07-30 11:38:44 +0600

    added company crawler (without implementation)

--- src/app.py
+++ src/app.py
@@ -1,29 +1,37 @@
 import uvicorn
+from http import HTTPStatus
 
-from fastapi import FastAPI, Request, status, Query
+from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 from src.models.user import User
 from src.models.company import Company
-from src.service.crawler import LICrawler
+from src.service.crawler_user import LIUserCrawler
+from src.utils.err_utils import ApplicationError
 
 app = FastAPI()
 
 
 @app.get('/profile', description='Get profile info')
 async def get(user_url: str):
-    parser = LICrawler()
-    _user: User = parser.get_user(user_link=user_url)
-    return JSONResponse(
-        content=jsonable_encoder({'data': _user})
-    )
+    try:
+        parser = LIUserCrawler()
+        _user: User = parser.get_user(user_link=user_url)
+        return JSONResponse(
+            content=jsonable_encoder({'data': _user})
+        )
+    except ApplicationError as e:
+        return JSONResponse(
+            status_code=HTTPStatus.BAD_REQUEST,
+            content=jsonable_encoder({'error': e.__dict__})
+        )
 
 
 @app.get('/company', description='Get company info')
 async def get(company_link: str):
     parser = LICrawler()
-    _company: Company = parser.get_company(user_link=company_link)
+    _company: Company = parser.get_company(company_link=company_link)
     return JSONResponse(
         content=jsonable_encoder({'data': _company})
     )
--- src/models/user.py
+++ src/models/user.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 
 class User(BaseModel):
     full_name: str
     location: str
     heading: Optional[str]
-    education: Optional[list]
-    experience: Optional[list]
-    skills: Optional[list]
+    education: Optional[List[dict]]
+    experience: Optional[List[dict]]
+    skills: Optional[List[dict]]
     profile_pic_url: Optional[str]
     user_url: str
