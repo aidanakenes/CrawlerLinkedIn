bbdd285262ca46758f16839bbed3b0887e33e26b
Aidana Kenes <aidanakenesova30@gmail.com>
2020-08-02 15:35:43 +0600

    💧 MinorUpdate

--- src/models/user.py
+++ src/models/user.py
@@ -5,10 +5,10 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     full_name: str
+    user_url: str
+    profile_pic_url: Optional[str]
     location: str
     heading: Optional[str]
     education: List[dict]
     experience: List[dict]
     skills: List[str]
-    profile_pic_url: Optional[str]
-    user_url: str
