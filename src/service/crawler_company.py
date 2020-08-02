0fc30897e800c408d0d037f80badfe4c8c9b49a3
Aidana Kenes <aidanakenesova30@gmail.com>
2020-08-02 15:33:05 +0600

    ✂️ Refactor: added try-except blocks

--- src/service/crawler_company.py
+++ src/service/crawler_company.py
@@ -1,45 +1,67 @@
-from bs4 import BeautifulSoup
+import json
+from typing import Optional
 
+import requests
+from pydantic.error_wrappers import ValidationError
 
-from src.service.crawler import LICrawler
 from src.utils.logger import get_logger
+from src.utils.err_utils import ApplicationError
 from src.models.company import Company
-from src.models.post import Post
-from src.utils.err_utils import BeautifulSoupError
+from src.utils.conf import HEADERS, COOKIES, COMPANY_PARAMS
 
 logger = get_logger(__name__)
 
 
-class LICompanyCrawler(LICrawler):
+class LICompanyCrawler:
 
     def __init__(self):
-        super().__init__()
-        self.driver = super().login()
+        self.li_comp_home = 'https://www.linkedin.com/company/'
+        self._request_url = 'https://www.linkedin.com/voyager/api/organization/companies'
 
-    def get_company(self, company_url: str):
-        self.driver.get(url=company_url)
-        html_page = self.driver.page_source
-        self.driver.close()
-
-        logger.info(f"Starting the LICompanyCrawler for user {company_url}")
+    def _get_company_data_(self, company_url: str):
+        COMPANY_PARAMS.append(('universalName', company_url[len(self.li_comp_home):-1]))
         try:
-            soup = BeautifulSoup(html_page, 'lxml')
-            logger.info(f"Returning the LICompanyCrawler's result for user {company_url}")
-            return Company(
-                title='',
-                heading='',
-                external_url=' ',
-                location='',
-                employees_num=0,
-                followers_num=0,
-                posts=[Post()],
-                url=company_url,
-                profile_pic_url=''
+            r = requests.get(
+                self._request_url,
+                headers=HEADERS,
+                params=COMPANY_PARAMS,
+                cookies=COOKIES,
+                timeout=10
             )
-        except TypeError as e:
-            logger.info(f'Cannot parse page: {type(e)}')
-            raise e
-        except Exception as e:
-            logger.info(f'Cannot parse page: {type(e)}')
-            raise BeautifulSoupError()
+            if r.ok:
+                return json.loads(r.text).get('included')
+        except TimeoutError as e:
+            logger.error(f'Failed to parse for company {company_url}: {type(e)}')
+            raise ApplicationError()
 
+    def get_company(self, company_url: str) -> Optional[Company]:
+        data = self._get_company_data_(company_url=company_url)
+        try:
+            for d in data:
+                major = d.get('localizedName') if 'localizedName' in d.keys() else None
+
+                if 'staffCount' in d.keys():
+                    pic_root_url = d.get('logo').get('image').get('rootUrl')
+                    pic_size = d.get('logo').get('image').get('artifacts')[-1].get('fileIdentifyingUrlPathSegment')
+
+                    logger.info(f"Returning the LICompanyCrawler's result for company {company_url}")
+                    return Company(
+                        title=d.get('name'),
+                        url=company_url,
+                        external_url=d.get('companyPageUrl'),
+                        logo=f'{pic_root_url}{pic_size}',
+                        major=major,
+                        employees_num=d.get('staffCount'),
+                        locations=[
+                            f"{loc.get('country')}, {loc.get('geographicArea')}, {loc.get('line1')}"
+                            for loc in d.get('confirmedLocations')
+                        ],
+                        heading=d.get('tagline'),
+                        about=d.get('description')
+                    )
+        except AttributeError as e:
+            logger.error(f'Failed to parse company: {type(e)}')
+            raise ApplicationError()
+        except ValidationError as e:
+            logger.error(f'Failed to parse company: {type(e)}')
+            raise ApplicationError()
--- src/service/crawler_post.py
+++ src/service/crawler_post.py
@@ -1,28 +1,65 @@
-import requests
+import json
+from typing import List
 
-from src.utils.conf import HEADERS, COOKIES
+import dateparser
+import requests
+from pydantic.error_wrappers import ValidationError
+
+from src.utils.conf import HEADERS, COOKIES, POST_PARAMS
 from src.utils.logger import get_logger
+from src.utils.err_utils import ApplicationError
+from src.models.post import Post
 
 logger = get_logger(__name__)
 
 
-class LICrawler:
+class LIPostCrawler:
     def __init__(self):
-        self.li_home = 'https://www.linkedin.com/uas/login'
+        self._request_url = 'https://www.linkedin.com/voyager/api/organization/updatesV2'
 
-    def login(self):
-        params = (
-            ('companyIdOrUniversalName', '9212433'),
-            ('count', '10'),
-            ('moduleKey', 'ORGANIZATION_MEMBER_FEED_DESKTOP'),
-            ('numComments', '0'),
-            ('numLikes', '0'),
-            ('paginationToken', '834516713-1596340853617-fa791986bd130276ff6ff2887bac3fc9'),
-            ('q', 'companyRelevanceFeed'),
-            ('start', '3'),
-        )
+    def _get_post_data_(self, company_id):
+        POST_PARAMS.append(('companyIdOrUniversalName', company_id))
+        try:
+            r = requests.get(
+                self._request_url,
+                headers=HEADERS,
+                params=POST_PARAMS,
+                cookies=COOKIES,
+                timeout=10
+            )
+            if r.ok:
+                return json.loads(r.text).get('included')
+        except TimeoutError as e:
+            logger.error(f'Failed to parse posts for {company_id}: {type(e)}')
+            raise ApplicationError()
 
-        response = requests.get('https://www.linkedin.com/voyager/api/organization/updatesV2', headers=HEADERS,
-                                params=params, cookies=COOKIES)
-        print(response.text)
+    def get_post(self, company_id: str) -> List[Post]:
+        data = self._get_post_data_(company_id=company_id)
+        posts = []
+        try:
+            for d in data:
+                if 'annotation' in d.keys():
+                    posts.append(
+                        Post(
+                            company_name=company_id,
+                            date=dateparser.parse(
+                                d.get('actor').get('subDescription').get('accessibilityText'),
+                                settings={'TIMEZONE': 'Asia/Almaty'},
+                                date_formats=['%d %B %Y']
+                            ),
+                            content=d.get('commentary').get('text').get('text')
+                        )
+                    )
+            logger.info(f"Returning the LIPostCrawler's result for posts of {company_id}")
+            return posts
+        except AttributeError as e:
+            logger.error(f'Failed to parse posts: {type(e)}')
+            raise ApplicationError()
+        except ValidationError as e:
+            logger.error(f'Failed to parse posts: {type(e)}')
+            raise ApplicationError()
 
+
+if __name__ == '__main__':
+    c = LIPostCrawler()
+    print(len(c.get_post(company_id='kolesagroup')))
