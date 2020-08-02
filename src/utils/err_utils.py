259757e891acbd51adf8a7d8602ee281122ec518
Aidana Kenes <aidanakenesova30@gmail.com>
2020-08-02 15:38:38 +0600

    ✂️ Refactor: removed unused Exceptions

--- src/utils/err_utils.py
+++ src/utils/err_utils.py
@@ -1,14 +1,2 @@
 class ApplicationError(Exception):
     pass
-
-
-class RegexError(ApplicationError):
-    pass
-
-
-class AuthenticationError(ApplicationError):
-    pass
-
-
-class BeautifulSoupError(ApplicationError):
-    pass
