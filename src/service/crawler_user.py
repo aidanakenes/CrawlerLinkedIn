763f7fd224f5fea31b47777706f3fc282b815c77
Aidana Kenessova <67684527+aidanakenes@users.noreply.github.com>
2020-08-02 14:15:18 +0600

    🔥MajorUpdate: rewrote methods w/o selenium

--- src/service/crawler_user.py
+++ src/service/crawler_user.py
@@ -1,120 +1,101 @@
-import re
 import json
 
-from typing import Optional, List
-from bs4 import BeautifulSoup
+import requests
+from typing import Optional, List, Dict
 
-from src.service.crawler import LICrawler
 from src.models.user import User
 from src.utils.logger import get_logger
-from src.utils.err_utils import RegexError, BeautifulSoupError
+from src.utils.conf import HEADERS, COOKIES, USER_PARAMS
 
 logger = get_logger(__name__)
 
 
-class LIUserCrawler(LICrawler):
+class LIUserCrawler:
 
     def __init__(self):
-        super().__init__()
         self.li_user_home = 'https://www.linkedin.com/in/'
-        self.driver = super().login()
+        self._request_url = 'https://www.linkedin.com/voyager/api/identity/dash/profiles'
+
+    def _get_user_data_(self, user_url: str):
+        USER_PARAMS.append(('memberIdentity', user_url[len(self.li_user_home):-1]))
+        response = requests.get(
+            self._request_url,
+            headers=HEADERS,
+            params=USER_PARAMS,
+            cookies=COOKIES
+        )
+        return json.loads(response.text)['included']
 
     @staticmethod
-    def get_education(soup: BeautifulSoup) -> List[dict]:
+    def _get_education_(data: dict) -> List:
         education = []
-        blocks = soup.findAll('li', attrs={'class': 'pv-education-entity'})
-        if blocks:
-            for block in blocks:
-                soup = BeautifulSoup(str(block), 'lxml')
-                school_name = soup.find('h3', attrs={'class': 'pv-entity__school-name t-16 t-black t-bold'})
-                degree_name = soup.find('span', attrs={'class': 'pv-entity__comma-item'})
-
-                school_name = str(school_name.text).strip() if school_name else None
-                degree_name = str(degree_name.text).strip() if degree_name else None
-
-                date_range = []
-                time = soup.findAll('time')
-                if time:
-                    date_range = [str(year).strip('<time></time>') for year in soup.findAll('time')]
-
+        for d in data:
+            if 'schoolUrn' in d.keys():
                 education.append({
-                    'school_name': school_name,
-                    'degree_name': degree_name,
-                    'date_range': date_range
+                    'school_name': d.get('schoolName'),
+                    'degree_name': d.get('degreeName'),
+                    'duration': [
+                        d.get('dateRange').get('start').get('year'),
+                        d.get('dateRange').get('end').get('year')
+                    ]
                 })
         return education
 
     @staticmethod
-    def get_experience(html_page: str) -> List[dict]:
-        regex = r'(?<="dateRange":\{"start":).*(?=,{"\*profilePositionInPositionGroup")'
-        try:
-            regex_res = re.findall(pattern=regex, string=html_page)
-            if regex_res:
-                regex_res = json.loads('[{"dateRange":{"start":' + regex_res[0] + ']')
-        except Exception as e:
-            logger.info(f'Cannot parse experience: {type(e)}')
-            raise RegexError()
-
+    def _get_experience_(data: dict) -> List:
         experience = []
-        for r in regex_res:
-            if r.get('companyName'):
-                date_range = r.get('dateRange')
-                start = date_range.get('start').get('year') if 'start' in date_range.keys() else None
-                end = date_range.get('end').get('year') if 'end' in date_range.keys() else None
-                experience.append({
-                    'company_name': r.get('companyName'),
-                    'job_title': r.get('title'),
-                    'date_range': {
-                        'start': start,
-                        'end': end
-                    }
-                })
-
+        for d in data:
+            if 'title' in d.keys():
+                company = d.get('companyName')
+                if company:
+                    start = d.get('dateRange').get('start')
+                    end = d.get('dateRange').get('end')
+                    experience.append({
+                        'company': d.get('companyName'),
+                        'job_title': d.get('title'),
+                        'duration': [
+                            start.get('year') if start else None,
+                            end.get('year') if end else None
+                        ]
+                    })
         return experience
 
     @staticmethod
-    def get_skills(html_page: str) -> List[str]:
+    def _get_skills_(data: dict) -> List:
         skills = []
-        regex = r'(?<={"entityUrn":"urn:li:fsd_skill:).*(?=Skill"\},)'
-        try:
-            regex_res = re.findall(pattern=regex, string=html_page)
-            if regex_res:
-                regex_res = json.loads('[{"entityUrn":"urn:li:fsd_skill:' + regex_res[0] + 'Skill"}]')
-        except Exception as e:
-            logger.info(f'Cannot parse skills: {type(e)}')
-            raise RegexError()
-
-        for skill in regex_res:
-            skills.append(skill.get('name'))
+        for d in data:
+            if 'fsd_skill' in d.get('entityUrn'):
+                skills.append(d.get('name'))
         return skills
 
-    def get_user(self, username: str) -> Optional[User]:
-        user_url = f'{self.li_user_home}{username}'
-        self.driver.get(url=user_url)
-        html_page = self.driver.page_source
-        self.driver.close()
+    @staticmethod
+    def _get_personal_(data: dict) -> Dict:
+        personal = {}
+        for d in data:
+            profile_pic_url = d.get('profilePicture')
+            if 'birthDateOn' in d.keys():
+                if profile_pic_url:
+                    root_url = d.get('profilePicture').get('displayImageReference').get('vectorImage').get('rootUrl')
+                    size = d.get('profilePicture').get('displayImageReference').get('vectorImage').get('artifacts')[
+                        -1].get(
+                        'fileIdentifyingUrlPathSegment')
+                    profile_pic_url = f'{root_url}{size}'
 
-        try:
-            soup = BeautifulSoup(html_page, 'lxml')
-            profile_pic_url: str = soup.find('img', attrs={'class': 'EntityPhoto-circle-9'})['src']
+                personal = {
+                    'full_name': f"{d.get('firstName')}{d.get('lastName')}",
+                    'heading': d.get('headline'),
+                    'last_name': d.get('lastName'),
+                    'location': d.get('locationName'),
+                    'profile_pic_url': profile_pic_url
+                }
+        return personal
 
-            logger.info(f"Returning the LIUserCrawler's result for user {user_url}")
-            return User(
-                full_name=str(
-                    soup.find('li', attrs={'class': 'inline t-24 t-black t-normal break-words'}).text
-                ).strip(),
-                location=str(soup.find('li', attrs={'class': 't-16 t-black t-normal inline-block'}).text).strip(),
-                heading=str(soup.find('h2', attrs={'class': 'mt1'}).text).strip(),
-                education=self.get_education(soup=soup),
-                experience=self.get_experience(html_page=html_page),
-                skills=self.get_skills(html_page=html_page),
-                profile_pic_url=None if 'data:image' in profile_pic_url else profile_pic_url,
-                user_url=user_url
-            )
-        except TypeError as e:
-            logger.info(f'Cannot parse page: {type(e)}')
-            raise e
-        except Exception as e:
-            logger.info(f'Cannot parse page: {type(e)}')
-            raise BeautifulSoupError()
-
+    def get_user(self, user_url: str) -> Optional[User]:
+        data = self._get_user_data_(user_url=user_url)
+        logger.info(f"Returning the LIUserCrawler's result for user {user_url}")
+        user_dict = self._get_personal_(data=data)
+        user_dict['user_url'] = user_url
+        user_dict['education'] = self._get_education_(data=data)
+        user_dict['experience'] = self._get_experience_(data=data)
+        user_dict['skills'] = self._get_skills_(data=data)
+        return User(**user_dict)
